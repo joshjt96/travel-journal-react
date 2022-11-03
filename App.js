d78d0d8ea0ad43732161dd0eea98a22b8a35e646
Josh Talley <jtdev84@gmail.com>
2022-11-03 10:46:44 +0000

    Add .map() and props to Section and App components

--- App.js
+++ App.js
@@ -2,15 +2,27 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Navbar from "./components/Navbar"
 import Section from "./components/Section"
+import data from "./data"
 
 // import { createRoot } from 'react-dom/client';
 
 function App() {     
     
+    const sections = data.map(item => {
+        return (
+            <Section 
+                    key={item.id}
+                    item={item}
+            />
+        )
+    })
+    
     return (
         <div>
             <Navbar />
-            <Section />
+            <section className="section">
+                {sections}
+            </section>
         </div>
     )
 }
--- components/Section.js
+++ components/Section.js
@@ -3,16 +3,16 @@ import React from "react"
 function Section(props) {
     return (
         <div className="section">
-            <img src="https://source.unsplash.com/WLxQvbMyfas" className="section-img"></img>
+            <img src={props.item.imageUrl} className="section-img"></img>
             <div className="section-right">
                 <div className="section-right-top">
                     <img src="./images/pin.png" className="pin"></img>
-                    <p className="country">Japan</p>
-                    <span className="google-maps-link">View on Google Maps</span>
+                    <p className="country">{props.item.country}</p>
+                    <span><a href={props.item.googleMapsUrl}><p className="google-maps-link">View on Google Maps</p></a></span>
                 </div>
-                <h2 className="location">Mount Fuji</h2>
-                <span className="date">12th Jan, 2021 - 24th Jan, 2021</span>
-                <p className="description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h2 className="location">{props.item.location}</h2>
+                <span className="date">{props.item.startDate} - {props.item.endDate}</span>
+                <p className="description">{props.item.description}</p>
                 </div>
         </div>
     )
