61121afdad55bb227dbba54ad2556dec49c14b7d
Josh Talley <jtdev84@gmail.com>
2022-11-03 09:27:51 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+// import { createRoot } from 'react-dom/client';
+
+function App() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,11 @@
+// Project requirements:
+// Data array in a seperate .js file: Title, location, Google Maps link, start date, end date, description, image URL
+// Use .map() and props
+// Styled and polished
+
+import React from "react"
+import ReactDOM from "react-dom"
+// import { createRoot } from 'react-dom/client';
+
+// ReactDOM.createRoot(document.getElementById("root")).render(App);
+ReactDOM.render(<App />, document.getElementById("root"))
